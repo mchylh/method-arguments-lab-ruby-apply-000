ea1bed41fa5a86f138f5a9a0e2f7562866e431e2
Michaylah Mam <mchylh@gmail.com>
2020-03-05 15:22:05 +0000

    Automatically backed up by Learn

--- lib/introduction.rb
+++ lib/introduction.rb
@@ -3,3 +3,9 @@
 def introduction(name)
   puts "Hi, my name is #{name}."
 end
+
+#introduction_with_language
+
+def introduction_with_language(name)
+  puts "Hi, my name is #{name} and I am learning to program in #{language}."
+end
